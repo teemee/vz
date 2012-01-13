789d6c495e225507e68c02df9693c4e11d863f24
Tim Schöllhammer <github@teemee.de>
2012-01-13 17:14:18 +0100

    - switched to non-anamorphic 16x9 (anamorphic files have -ana at the end)
    - new Background (old files have _hoch at the end)

---
+++ _vz-autorun.bat
@@ -0,0 +1,10 @@
+@ECHO off
+:menu
+cls
+ECHO **************************************************
+ECHO *                  Vz Autoloader                 *
+ECHO **************************************************
+ECHO.
+ECHO.
+ECHO 1. Decklink Component + Guten Morgen Filstal
+vz.exe -f ./projects/guten-morgen-filstal/guten-morgen-filstal.xml -c ./decklink-component.conf
